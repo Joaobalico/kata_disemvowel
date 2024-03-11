cb6d61408005da06bbfc9888717851103ed89f64
João Bálico <joao.balico@metyis.com>
2024-03-11 21:11:39 +0000

    kata solved

--- main.py
+++ main.py
@@ -7,3 +7,22 @@
 # For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
 # Note: for this kata y isn't considered a vowel.
+
+def disemvowel(string_):
+    vowels = [65, 69, 73, 79, 85, 97, 101, 105, 111, 117]
+    list_from_str = list(string_)
+
+    for i in list_from_str:
+        if ord(i) in vowels:
+            list_from_str.remove(i)
+            for i in list_from_str:
+                if ord(i) in vowels:
+                    list_from_str.remove(i)
+    str_from_list= ''.join(list_from_str)
+    return print(str_from_list)
+
+
+
+
+disemvowel("This websitetU is for losers LOL!") #"Ths wbst s fr lsrs LL!"
+disemvowel("Your No offense but, Your writing is among the worst I've ever read") #"Ths wbst s fr lsrs LL!"
